b0620f5154eafa50c8c040f4092c9f5a5d6b69c6
nilton kazuo <hajikazuo@outlook.com>
2022-07-03 16:42:50 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio4 avaliacao logica de programacao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4_avaliacao_logica_de_programacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Utilizando estrutura FOR, escrever um programa para exibir os números de 1 até 50 na tela.
+
+
+            for (int i = 1; i <= 50; i++)
+                Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
